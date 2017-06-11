0d4a4670dc145e10a49dcfa6cb0e4dc79944a920
Yiqing Zhu <sbbird.zhu@gmail.com>
2017-06-11 14:26:49 +0900

    add thunk and logging

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 // Create final store using all reducers and applying middleware
 import { createBrowserHistory } from 'history';
 // Redux utility functions
+import thunk from 'redux-thunk';
 import { compose, createStore, combineReducers, applyMiddleware } from 'redux';
 import { routerMiddleware, connectRouter } from 'connected-react-router';
 // Import all reducers
 import * as reducers from 'reducers';
+import { createLogger } from 'redux-logger';
+
 
 // Configure reducer to store state at state.router
 // You can store it elsewhere by specifying a custom `routerStateSelector`
@@ -12,10 +15,21 @@ import * as reducers from 'reducers';
 export const history = createBrowserHistory();
 const reducer = combineReducers({ ...reducers });
 
+const middleware = [
+  thunk,
+  routerMiddleware(history)
+];
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
+
 const store = compose(
   // Enables your middleware:
   // applyMiddleware(thunk), // any Redux middleware, e.g. redux-thunk
-  applyMiddleware(routerMiddleware(history)),
+  applyMiddleware(
+      ...middleware
+  ),
   // Provides support for DevTools via Chrome extension
   window.devToolsExtension ? window.devToolsExtension() : f => f
 )(createStore)(connectRouter(history)(reducer));
